cf24a676cc40858e2c36e7d03607476ffd93c514
EmmanuelAkigbogun <105459628+EmmanuelAkigbogun@users.noreply.github.com>
2023-04-15 10:11:44 +0100

    Add files via upload

--- light_hall_count_star.js
+++ light_hall_count_star.js
@@ -1,106 +1,127 @@
-            staticObject={};//initializing an object to store data
-            let countAll=0;//initializing  the countAll overall count made
-            //s is element clicked 
-            //function calle when you click each flexowrap button with a (background image)
-            clickFunction=(s)=>{
-                countAll=0;
+            staticObject={};
+            //initializing an object to store data
+            let total=0;
+            //initializing  the total overall count made
+            //s is element clicked
+            //function calle when you click each grid_items button with a (background image)
+            f=(s)=>{
+                total=0;
                 //number for counting similar class
                 static_count=0;
-                document.querySelectorAll('.flexowrap').forEach(el=>{
+                document.querySelectorAll('.grid_items').forEach(el=>{
                     //when a  flexowrap is not defined in the new object
-                    if(staticObject['counting'+static_count]==undefined){
-                        staticObject['counting'+static_count]=0;
+                    if(staticObject['object_tracker'+static_count]==undefined){
+                        staticObject['object_tracker'+static_count]=0;
                     }
-                    //s is element clicked and el is for each element flexowrap 
+                    //s is element clicked and el is for each element grid_items
                     if(el==s){
                         //when equalcount is increased and element unique number stored in staticObject
                         //button number is increased
                         //and value is stored to be obtained incase of refresh
-                        staticObject['counting'+static_count]=Number(staticObject['counting'+static_count])+1;
-                        document.querySelectorAll('.number')[static_count].innerHTML=staticObject['counting'+static_count];
-                        window.localStorage.setItem('counting'+static_count,staticObject['counting'+static_count]);
-                    }//increasing number of the flexowrap clicked
-                    countAll+=Number(window.localStorage.getItem('counting'+static_count,));//countAll vote count
-                    static_count++;//count for each element 
-                    
+                        staticObject['object_tracker'+static_count]=Number(staticObject['object_tracker'+static_count])+1;
+                        document.querySelectorAll('.item_count')[static_count]
+                        .innerHTML=document.querySelectorAll('.item_count')[static_count]
+                        .innerHTML.replace(document.querySelectorAll('.item_count')[static_count]
+                        .innerText,staticObject['object_tracker'+static_count]);
+                        window.localStorage
+                        .setItem('object_tracker'+static_count,staticObject['object_tracker'+static_count]);
+                    }
+                    //increasing number of the flexowrap clicked
+                    total+=Number(window.localStorage
+                    .getItem('object_tracker'+static_count,));
+                    //total vote count
+                    static_count++;
+                    //count for each element
                 })
-                countAll_count.innerHTML = `Total: ${countAll}`;
-        }
-          /*everytime page loads*/
-            flexowrapnumber=0;            
-            document.querySelectorAll('.flexowrap').forEach(el=>{
-                 /*allbutton are given a click event*/
+                total_count.innerHTML = ` ${total} `;
+            }
+            /*everytime page loads seting required definitions*/
+            flexowrapnumber=0;
+            document.querySelectorAll('.grid_items').forEach(el=>{
+                /*allbutton are given a click event*/
                 el.onclick=(e)=>{
-                    clickFunction(e.target);      
-                    notification.style.backgroundImage=
-                    "url(./images/"+e.target.style.backgroundImage
-                    .split('/')[e.target.style.backgroundImage.split('/').length-1]
-                    .replaceAll('"',"").replaceAll(')',"")+")";
-                    file_name.innerHTML=e.target.style.backgroundImage
-                    .split('/')[e.target.style.backgroundImage.split('/').length-1]
+                    f(e.target);
+                    file_name.innerHTML="Last Clicked Image :<wbr> "+getComputedStyle(e.target).backgroundImage
+                    .split('/')[getComputedStyle(e.target).backgroundImage.split('/').length-1]
                     .replaceAll('"',"").replaceAll(')',"").replaceAll(".jpg","").replaceAll(".jpeg","")
                 }
-                /*all count divs are created and appended to  button*/
-                elementNumber=document.createElement('div');
-                el.appendChild(elementNumber);
-                elementNumber.className="number";
-                 /*all count divs are checked if they have data from the past*/
-                if(window.localStorage.getItem('counting'+flexowrapnumber)!=undefined){
-                     /*past data are restored*/
-                    elementNumber.innerHTML=window.localStorage.getItem('counting'+flexowrapnumber)
-                    staticObject['counting'+flexowrapnumber]=window.localStorage.getItem('counting'+flexowrapnumber)
+                /*all count output and icons are created and appended to  button*/
+                love=document.createElement('output');
+                love_img=new Image
+                love_img.src="images/love.png";
+                love_img.setAttribute("alt","love icon");
+                el.appendChild(love);
+                love.appendChild(love_img);
+                love.className="item_count";
+                /*all count divs are checked if they have data from the past*/
+                if(window.localStorage.getItem('object_tracker'+flexowrapnumber)!=undefined){
+                    /*past data are restored*/
+                    love.innerHTML=love.innerHTML.replace(love.innerText,window.localStorage
+                    .getItem('object_tracker'+flexowrapnumber))
+                    staticObject['object_tracker'+flexowrapnumber]=window.localStorage
+                    .getItem('object_tracker'+flexowrapnumber);
+                    total+=Number(window.localStorage.getItem('object_tracker'+flexowrapnumber))
                 }
                 else{
-                 /*without past data divs start count number*/
-                elementNumber.innerHTML="0";}
+                    /*without past data divs start count number*/
+                    love.innerHTML=love.innerHTML.replace(love.innerText,"0");
+                }
                 flexowrapnumber++;
             })
-            
+            // geting total on reload
+            total_count.innerHTML=total;
+            //reset button
             /*for reseting all button images previously clicked*/
             resetFunction=()=>{
-            flexowrapnumber=0;            
-            document.querySelectorAll('.flexowrap').forEach(el=>{
-                 /*remove history*/
-                window.localStorage.removeItem('counting'+flexowrapnumber)
-                 /*start object*/
-                staticObject={}
-                /*initialize buttons*/
-                document.querySelectorAll('.number')[flexowrapnumber].innerHTML="0";
-                flexowrapnumber++;
-                // to prevent error after reset for not defining object and storage
-                clickFunction(document.querySelectorAll('.flexowrap')[0]);
-                staticObject['counting'+0]=0;
-                document.querySelectorAll('.number')[0].innerHTML="0"
-                 window.localStorage.setItem('counting'+0,staticObject['counting'+0]);
-            })
-            countAll=1;
-            countAll_count.innerHTML="Total: 0"
+                flexowrapnumber=0;
+                document.querySelectorAll('.grid_items').forEach(el=>{
+                    /*remove history*/
+                    window.localStorage.removeItem('object_tracker'+flexowrapnumber)
+                    /*start object*/
+                    staticObject={
+                    }
+                    /*initialize buttons*/
+                    document.querySelectorAll('.item_count')[flexowrapnumber]
+                    .innerHTML=document.querySelectorAll('.item_count')[flexowrapnumber]
+                    .innerHTML.replace(document
+                    .querySelectorAll('.item_count')[flexowrapnumber].innerText,"0");
+                    flexowrapnumber++;
+                    // to prevent error after reset for not defining object and storage
+                    f(document.querySelectorAll('.grid_items')[0]);
+                    staticObject['object_tracker'+0]=0;
+                    document.querySelectorAll('.item_count')[0]
+                    .innerHTML=document.querySelectorAll('.item_count')[0]
+                    .innerHTML.replace(document.querySelectorAll('.item_count')[0].innerText,"0");
+                    window.localStorage.setItem('object_tracker'+0,staticObject['object_tracker'+0]);
+                })
+                total=1;
+                total_count.innerHTML="0"
             }
-            addFunction=()=>{
-            flexowrapnumber=0;            
-            document.querySelectorAll('.flexowrap').forEach(el=>{
-                 /*increment all object*/
-                staticObject['counting'+flexowrapnumber]=Number(staticObject['counting'+flexowrapnumber])+1;
-                /*increment all hisory*/
-                 window.localStorage.setItem('counting'+flexowrapnumber,staticObject['counting'+flexowrapnumber]);
-                /*increment all buttons*/
-                document.querySelectorAll('.number')[flexowrapnumber].innerHTML=Number(
-                    document.querySelectorAll('.number')[flexowrapnumber].innerHTML
-                )+1;
-                flexowrapnumber++;
-            })
-             countAll_count.innerHTML = `Total: ${countAll+60}`;
-            }
-            
             //reset button function given to hamburger and reset all button
-            m[0].onclick=()=>{
-                  startOver()
+            reset.onclick=()=>{
+                resetFunction()
             }
-            hamburger.onclick=()=>{
-                 startOver()
+            //hamburger menu clicks
+            window.onclick=(el)=>{
+                console.log(el.target.id)
+                if(el.target==document.querySelector('#logo_container button')){
+                    if(document.querySelector('#logo_container button').id==""){
+                        document.querySelector('#logo_container button').setAttribute('id','icon_hamburger')
+                        menu_container_close.setAttribute('id','menu_container')
+                    }
+                    else if(document.querySelector('#logo_container button').id!=""){
+                        document.querySelector('#logo_container button').removeAttribute('id')
+                        menu_container.setAttribute('id','menu_container_close')
+                    }
+                }
+                else {
+                    if(document.body.innerHTML.includes('menu_container')){
+                        if(document.querySelector('#logo_container button').id!=""
+                        &&!menu_container.contains(el.target)
+                        &&(el.target.tagName!=='NAV')){
+                            document.querySelector('#logo_container button').removeAttribute('id')
+                            menu_container.setAttribute('id','menu_container_close')
+                        }
+                    }
+                }
             }
-             m[1].onclick=()=>{
-                 addFunction()
-            }
-            
-            
