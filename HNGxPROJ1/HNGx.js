e2c52b13e5e5e73baad0004585a69d4f41432293
EmmanuelAkigbogun <105459628+EmmanuelAkigbogun@users.noreply.github.com>
2023-09-11 18:28:50 +0100

    Delete HNGxPROJ1 directory

--- HNGxPROJ1/HNGx.js
+++
@@ -1,9 +0,0 @@
-const day = document.getElementById("day");
-const timeUTC = document.getElementById("time");
-const date = new Date();
-var dayValue = date.toLocaleString("en-us",{
-    weekday:"long"
-});
-var timeUTCValue = date.gettime();
-day.innerHTML=dayValue;
-timeUTC.innerHTML=timeUTCValue;
